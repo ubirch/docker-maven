469434aed2c954c702b989fe0327e0b3fd0b3fca
Falko Zurell <falko.zurell@ubirch.com>
2016-04-20 17:01:31 +0200

    fixed build.sh control flow
    Signed-off-by: Falko Zurell <falko.zurell@ubirch.com>

--- build.sh
+++ build.sh
@@ -16,8 +16,6 @@ fi
 echo "Building Maven container with JAVA_VERSION=${JAVA_VERSION} JAVA_UPDATE=${JAVA_UPDATE} JAVA_BUILD=${JAVA_BUILD}"
 
 mkdir -p VAR && docker build --build-arg JAVA_VERSION=${JAVA_VERSION:=8} \
-  --build-arg JAVA_UPDATE=${JAVA_UPDATE:=77} \
-  --build-arg JAVA_BUILD=${JAVA_BUILD:=03} \
   -t ubirch/maven-build .
 
 
@@ -25,4 +23,7 @@ if [ $? -eq 0 ]; then
     echo ${JAVA_VERSION:=8} > VAR/JAVA_VERSION
     echo ${JAVA_UPDATE:=77} > VAR/JAVA_UPDATE
     echo ${JAVA_BUILD:=03} > VAR/JAVA_BUILD
-fi
+else
+    echo "Docker build failed"
+    exit 1
+  fi
