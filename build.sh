83f03ee81056c7176dd20f0d8638cb00a96c1e7c
Falko Zurell <falko.zurell@ubirch.com>
2016-04-21 10:36:55 +0200

    added functions to reuse script in multiple stages

--- build.sh
+++ build.sh
@@ -1,13 +1,9 @@
 #!/bin/bash -x
 
-# check out out GO tools
-git clone https://github.com/ubirch/go-tools.git
-
-if [ $? -ne 0 ]; then exit 1; fi
 
 
 # calculate our new label for the version string
-NEW_LABEL=`./go-tools/concat-labels.sh`
+NEW_LABEL=""
 
 
 if [ -f VAR/JAVA_VERSION ]; then
@@ -22,20 +18,65 @@ if [ -f VAR/JAVA_UPDATE ]; then
   export JAVA_UPDATE=`cat VAR/JAVA_UPDATE`
 fi
 
+function fetch_got-tools() {
+    # check out out GO tools
+    git clone https://github.com/ubirch/go-tools.git
 
-echo "Building Maven container with JAVA_VERSION=${JAVA_VERSION} JAVA_UPDATE=${JAVA_UPDATE} JAVA_BUILD=${JAVA_BUILD}"
+    if [ $? -ne 0 ]; then exit 1; fi
+}
 
-mkdir -p VAR && docker build --build-arg JAVA_VERSION=${JAVA_VERSION:=8} \
-  -t ubirch/maven-build:v${NEW_LABEL} .
+function init() {
+  NEW_LABEL=`./go-tools/concat-labels.sh`
+}
+
+# build the docker container
+function build_container() {
+
+    echo "Building Maven container with JAVA_VERSION=${JAVA_VERSION} JAVA_UPDATE=${JAVA_UPDATE} JAVA_BUILD=${JAVA_BUILD}"
+
+    mkdir -p VAR && docker build --build-arg JAVA_VERSION=${JAVA_VERSION:=8} -t ubirch/maven-build:v${NEW_LABEL} .
 
 
-if [ $? -eq 0 ]; then
-    echo ${JAVA_VERSION:=8} > VAR/JAVA_VERSION
-    echo ${JAVA_UPDATE:=77} > VAR/JAVA_UPDATE
-    echo ${JAVA_BUILD:=03} > VAR/JAVA_BUILD
-    echo ${NEW_LABEL} > VAR/BUILD_LABEL
+    if [ $? -eq 0 ]; then
+        echo ${JAVA_VERSION:=8} > VAR/JAVA_VERSION
+        echo ${JAVA_UPDATE:=77} > VAR/JAVA_UPDATE
+        echo ${JAVA_BUILD:=03} > VAR/JAVA_BUILD
+        echo ${NEW_LABEL} > VAR/BUILD_LABEL
+        echo ${NEW_LABEL} > VAR/GO_PIPELINE_NAME_${GO_PIPELINE_NAME}
+    else
+        echo "Docker build failed"
+        exit 1
+    fi
+
+}
+
+# publish the new docker container
+function publish_container() {
+  echo "Publishing Docker Container with version: ${NEW_LABEL}"
+  docker push ubirch/maven-build:v${NEW_LABEL}
+
+  if [ $? -eq 0 ]; then
     echo ${NEW_LABEL} > VAR/GO_PIPELINE_NAME_${GO_PIPELINE_NAME}
-else
-    echo "Docker build failed"
+  else
+    echo "Docker push faild"
     exit 1
   fi
+
+}
+
+
+case "$1" in
+    build)
+        fetch_got-tools
+        init
+        build_container
+        ;;
+    publish)
+        publish_container
+        ;;
+    *)
+        echo "Usage: $0 {build|publish}"
+        exit 1
+esac
+
+exit 0
