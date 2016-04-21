e5279e00f604fc1aab678d1ecee8bd06c9019be9
Falko Zurell <falko.zurell@ubirch.com>
2016-04-21 13:48:29 +0200

    simplified build script
    don't need the go-tools at the moment
    
    Signed-off-by: Falko Zurell <falko.zurell@ubirch.com>

--- build.sh
+++ build.sh
@@ -1,11 +1,6 @@
 #!/bin/bash -x
 
 
-
-# calculate our new label for the version string
-NEW_LABEL=""
-
-
 if [ -f VAR/JAVA_VERSION ]; then
   export JAVA_VERSION=`cat VAR/JAVA_VERSION`
 fi
@@ -18,29 +13,8 @@ if [ -f VAR/JAVA_UPDATE ]; then
   export JAVA_UPDATE=`cat VAR/JAVA_UPDATE`
 fi
 
-function fetch_got-tools() {
-
-    if [ -d go-tools ]; then
-      # update the existing repo
-      cd go-tools
-      git pull
-      if [ $? -ne 0 ]; then exit 1; fi
-      cd ..
-    else
-      # check out out GO tools
-      git clone https://github.com/ubirch/go-tools.git
-      if [ $? -ne 0 ]; then exit 1; fi
-    fi
-
-
-}
-
-function init() {
-  NEW_LABEL=`./go-tools/concat-labels.sh`
-}
-
 function fix_dockerfile_version() {
-  sed "s#FROM ubirch/java#FROM ubirch/java:v${GO_DEPENDENCY_LABEL_JAVA_BASE_CONTAINER}#g" Dockerfile > Dockerfile.v${NEW_LABEL}
+  sed "s#FROM ubirch/java#FROM ubirch/java:v${GO_DEPENDENCY_LABEL_JAVA_BASE_CONTAINER}#g" Dockerfile > Dockerfile.v${GO_PIPELINE_LABEL}
 }
 
 # build the docker container
@@ -50,7 +24,7 @@ function build_container() {
 
     echo "Building Maven container with JAVA_VERSION=${JAVA_VERSION} JAVA_UPDATE=${JAVA_UPDATE} JAVA_BUILD=${JAVA_BUILD}"
 
-    mkdir -p VAR && docker build --build-arg JAVA_VERSION=${JAVA_VERSION:=8} -t ubirch/maven-build:v${NEW_LABEL} -f Dockerfile.v${NEW_LABEL} .
+    mkdir -p VAR && docker build --build-arg JAVA_VERSION=${JAVA_VERSION:=8} -t ubirch/maven-build:v${GO_PIPELINE_LABEL} -f Dockerfile.v${GO_PIPELINE_LABEL} .
 
 
     if [ $? -eq 0 ]; then
@@ -82,13 +56,9 @@ function publish_container() {
 
 case "$1" in
     build)
-        fetch_got-tools
-        init
         build_container
         ;;
     publish)
-        fetch_got-tools
-        init
         publish_container
         ;;
     *)
